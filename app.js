c7678d18285fc9d10da81baa716c825a3aabe573
Ross Karchner <ross.karchner@cfpb.gov>
2014-02-26 11:00:33 -0500

    JSON handler now enforces a single constraint:
    The submission must be an array. Also added example JSON to README

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 var express = require('express'), db  = require('./models');
 var app = express();
 
+app.use(express.json());
 
 app.post('/save', function(req, res){
     var req = req;
     if (req.is('json')){
-        res.json({success:true});
+        var submissions = req.body;
+        
+        if (Array.isArray(submissions)){ 
+            for (var i = 0; i < submissions.length; i++) {
+                    var submission = submissions[i]
+                    console.log([submission.x,submission.y,submission.text]);
+            }
+            res.json({success:true});
+        } else {
+            
+            res.json({success:false, message:"submission must be an array"});
+        }
+
     }
     res.json({success:false, message:"submission must be JSON"});
 });
