ae4182bda3facc5e02ab39836b938880ae619d76
Adam Scott <adam.scott@cfpb.gov>
2014-02-26 15:19:42 -0500

    sticky sidebar on scroll

--- assets/js/app.js
+++ assets/js/app.js
@@ -28,3 +28,18 @@ app.controller('MainCtrl', function ($scope, $rootScope) {
     }
 });
 
+app.directive("scroll", function ($window) {
+  return function(scope, element, attrs) {
+    angular.element($window).bind("scroll", function() {
+       if (this.pageYOffset >= 100) {
+        scope.boolChangeClass = true;
+        console.log('Scrolled below header.');
+       } else {
+        scope.boolChangeClass = false;
+        console.log('Header is in view.');
+       }
+      scope.$apply();
+    });
+  };
+});
+
