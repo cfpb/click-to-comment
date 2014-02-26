e29590db3baa0ce631c9b4d9d51d182f812c101a
Adam Scott <adam.scott@cfpb.gov>
2014-02-26 15:20:02 -0500

    remove console.log statements

--- assets/js/app.js
+++ assets/js/app.js
@@ -33,10 +33,8 @@ app.directive("scroll", function ($window) {
     angular.element($window).bind("scroll", function() {
        if (this.pageYOffset >= 100) {
         scope.boolChangeClass = true;
-        console.log('Scrolled below header.');
        } else {
         scope.boolChangeClass = false;
-        console.log('Header is in view.');
        }
       scope.$apply();
     });
